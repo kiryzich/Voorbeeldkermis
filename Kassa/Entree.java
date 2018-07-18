9f96a3edfb83630ed7a1aa7ca862931d92e050e4
Arne van Buel <arnevanbuel@hotmail.nl>
2018-07-18 11:07:13 +0200

    Update 5
    Lijstje gemaakt, alleen keuze 1 werkt momenteel maar dat is niet erg haha

--- Kassa/Entree.java
+++ Kassa/Entree.java
@@ -22,7 +22,7 @@ class Lijst { // deze lijst moet de andere lijsten aanroepen
     Lijst() {
         System.out.println("Welkom bij de kermis. Wat wilt u gaan doen?");
     }
-    String[] eten = {"Hamburger"};
+    String[] attractie = {"De botsauto's","De legendarische spin","Het spiegelpaleis","Het spookhuis","De hawaaiiiii","Het ladderzatklimmen"};
     int[] prijs = {10};
     void start(){
         System.out.println("Kies een nummer om verder te gaan");
@@ -31,28 +31,44 @@ class Lijst { // deze lijst moet de andere lijsten aanroepen
 
     void doorGaanMetBestellen() {
         System.out.print("Wilt u nog een bestelling doen? [ja] of [nee]");
+        System.out.println("");
         Scanner sc2 = new Scanner(System.in);
         String weer = sc2.nextLine();
         switch (weer) {
             case "ja":
+            case "Ja":
+            case "jA":
+            case "JA":   // 4 verschillende (2 letters op 2 maniers = 2^2 = 4 maniered) geschreven zodat de Ja hoe dan ook gepakt wordt!
                 System.out.println("U kunt uw volgende bestelling plaatsen!");
                 bestellen();
                 break;
             case "nee":
+            case "Nee":
+            case "NEE": // de andere spelfouten pakt de default maar hoor!
                 System.out.println("Bedankt voor uw bestelling!");
                 break;
-
+            default :
+                System.out.println("Dit is geen geldige keuze, probeer het opnieuw! [Derp Derp]");
+                doorGaanMetBestellen();
+                break;
         }
     }
     void bestellen(){
         System.out.println("Kies een product");
-        System.out.print("01 - Hamburger");
+        int x;
+        for(x=0; x < attractie.length; x++){
+            System.out.println((x+1) + " - " + attractie[x] + "");
+
+
+        }
+
+        System.out.println("TOT HIER <<<<<<<<<<<");
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
 
         switch (y){
             case 1: y = 0;
-                System.out.println("U keuze is een " + eten[0] + ", met een prijs van €" +prijs[0]);
+                System.out.println("U keuze is een " + attractie[0] + ", met een prijs van €" +prijs[0]);
                 doorGaanMetBestellen();
                     break;
             case 2: y = 1;
